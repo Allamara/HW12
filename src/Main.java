8bd31526b8c5dec9605e5d5e418b645faab1bd44
Anna <annanen@inbox.ru>
2023-11-12 00:02:33 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Author a1 = new Author("a1", "a2");
+        Author a2 = new Author("a3", "a4");
+        Book book1 = new Book("title1", 2022, a1);
+        Book book2 = new Book("title2", 2023, a2);
+
+        System.out.println(book1.getTitle() + " - " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + ". " + book1.getYear());
+        book1.setYear(2021);
+        System.out.println(book1.getYear());
+
+    }
+}
